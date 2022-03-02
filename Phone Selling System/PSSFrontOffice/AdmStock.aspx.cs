90dce88ff6f57702d6d4a2e9bb4bcd16020bcec1
Syed Naqvi <P2652259@my365.dmu.ac.uk>
2022-03-02 19:47:41 +0000

    Adm Stock Edited

--- Phone Selling System/PSSFrontOffice/AdmStock.aspx.cs
+++ Phone Selling System/PSSFrontOffice/AdmStock.aspx.cs
@@ -5,10 +5,28 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsStock AStock = new clsStock();
+        AStock = (clsStock)Session["AStock"];
+        Response.Write(AStock.StockName);
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsSupplier
+        clsStock AStock = new clsStock();
+        //capture the name
+        AStock.StockName = txtStock.Text;
+        //Store address in session object
+        Session["AStock"] = AStock;
+        //redirect to viewer page
+        Response.Redirect("SupplierViewer.aspx");
 
     }
+
 }
+
