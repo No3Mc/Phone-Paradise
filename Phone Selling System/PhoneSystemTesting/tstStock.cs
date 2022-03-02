97dd6a878c03985a2727c1e8ff7d313cfff32edf
Syed Naqvi <P2652259@my365.dmu.ac.uk>
2022-03-02 17:02:39 +0000

    Test Stock Class

---
+++ Phone Selling System/PhoneSystemTesting/tstStock.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PhoneSystemTesting
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
