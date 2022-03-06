6f5539b91f7f0ca311e606c4367d28508b5a8954
Syed Naqvi <P2652259@my365.dmu.ac.uk>
2022-03-06 00:59:48 +0000

    Lab 23 | Collection Class

---
+++ Phone Selling System/PhoneSystemTesting/tstStockCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSSClasses;
+
+
+
+namespace PhoneSystemTesting
+{
+
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStockCollection AllStocks = new clsStockCollection();
+            //test to see that it exist;
+            Assert.IsNotNull(AllStocks);
+        }
+    }
+}
