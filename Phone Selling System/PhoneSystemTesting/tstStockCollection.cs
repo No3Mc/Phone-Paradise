4d70dcbd118036f2a534101a03947fb0a6b450fe
Syed Naqvi <P2652259@my365.dmu.ac.uk>
2022-03-06 01:23:20 +0000

    Week 23 | Stock Collection Tests

--- Phone Selling System/PhoneSystemTesting/tstStockCollection.cs
+++ Phone Selling System/PhoneSystemTesting/tstStockCollection.cs
@@ -22,5 +22,56 @@ namespace PhoneSystemTesting
             //test to see that it exist;
             Assert.IsNotNull(AllStocks);
         }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            //create an instance of the class we want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list objects
+            List<clsStock> TestList = new List<clsStock>();
+            //add an item to the list
+            //create the item of test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.StockID = 1;
+            TestItem.StockName = "a";
+            TestItem.WarehouseNo = "a";
+            TestItem.Location = "a";
+            TestItem.Quantity = "a";
+            TestItem.Barcode = "a";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            AllStock.StockList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStock.StockList, TestList);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
