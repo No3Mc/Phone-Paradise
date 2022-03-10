8321ee6243e6082ac10f677e790e45888289ea85
Syed Naqvi <P2652259@my365.dmu.ac.uk>
2022-03-10 00:25:06 +0000

    Weeek 23 | Test

--- Phone Selling System/PSSFrontOffice/Stock/StockViewer.aspx.cs
+++ Phone Selling System/PSSFrontOffice/Stock/StockViewer.aspx.cs
@@ -4,11 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using PSSClasses;
 
 public partial class StockViewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        ////if this is the first time the page is displayed
+        //if (IsPostBack == false)
+        //{
+        //    //update the list box
+        //    DisplayStocks();
+        //}
+
+
+        //void DisplayStocks()
+        //{
+        //    //create an instance of the county collection
+        //    PSSClasses.clsStockCollection Stocks = new PSSClasses.clsStockCollection();
+        //    //set the data source to the list of warehouse in the collection
+        //    lstStocks.DataSource = Stocks.StockList;
+        //    //set the name of the primary key
+        //    lstStocks.DataValueField = "stockid";
+        //    //set the data field to display
+        //    lstStocks.DataTextField = "warehouseno";
+        //    //bind the data to the list
+        //    lstStocks.DataBind();
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
+
+
+
 }
