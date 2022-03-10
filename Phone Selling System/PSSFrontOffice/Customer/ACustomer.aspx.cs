73fa341e189de1801c4166567580f65fb438a0c2
Mahammad Memon <P2629099@my365.dmu.ac.uk>
2022-03-10 02:58:45 +0000

    Week 19 Done

---
+++ Phone Selling System/PSSFrontOffice/Customer/ACustomer.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PSSClasses;
+
+
+public partial class ACustomer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
+        //capture the customer id
+        ACustomer.Name = txtName.Text;
+        //store the name in the session object
+        Session["ACustomer"] = ACustomer;
+        //redirect to the viwer page
+        Response.Redirect("CustomerViewer.aspx");
+
+    }
+}
