0cdbc30863ba89a8afdfd02be1159207a5c5bffb
Mahammad Memon <P2629099@my365.dmu.ac.uk>
2022-03-14 14:54:28 +0000

    Found Methods OK

--- Phone Selling System/PSSClasses/Customer/clsCustomer.cs
+++ Phone Selling System/PSSClasses/Customer/clsCustomer.cs
@@ -1,12 +1,165 @@
-﻿namespace PSSClasses
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data;
+
+
+
+
+
+
+
+namespace PSSClasses
 {
+    //Dbo.tblCustomer Values
+    //[CustID]
+    //[Address]
+    //[DOB]     
+    //[Name]  
+    //[Email] 
+    //[PhoneNo]
     public class clsCustomer
     {
-        public string Address { get; set; }
-        public string DOB { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneN { get; set; }
-        public int CustID { get; set; }
+        private int mCustID;
+        private string mAddress;
+        private string mDOB;
+        private string mName;
+        private string mEmail;
+        private string mPhoneNo;
+
+
+
+        public string Address
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mAddress;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mAddress = value;
+            }
+
+        }
+
+
+        public string DOB
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mDOB;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDOB = value;
+            }
+
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mName = value;
+            }
+
+        }
+ 
+
+        public string Email
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mEmail;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mEmail = value;
+            }
+
+        }
+
+
+        public string PhoneNo
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mPhoneNo;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mPhoneNo = value;
+            }
+
+        }
+
+        public int CustID
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mCustID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mCustID = value;
+            }
+
+        }
+        public bool Find(int CustID)
+        {
+            //create an instance of the data connectin
+            clsDataConnection DB = new clsDataConnection();
+            //add the parametrs for the book to search for it
+            DB.AddParameter("@CustID", CustID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblStock_FilterByCustID");
+
+
+            //if one record if found (there should either one or zero)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mCustID = Convert.ToInt32(DB.DataTable.Rows[0]["CustID"]);
+                mAddress = Convert.ToString(DB.DataTable.Rows[0]["Address"]);
+                mDOB = Convert.ToString(DB.DataTable.Rows[0]["DOB"]);
+                mName = Convert.ToString(DB.DataTable.Rows[0]["Name"]);
+                mEmail = Convert.ToString(DB.DataTable.Rows[0]["Email"]);
+                mPhoneNo = Convert.ToString(DB.DataTable.Rows[0]["PhoneNo"]);
+
+                //return everyting worked OK
+                return true;
+            }
+
+
+
+            //if no records was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
+
+        }
+
     }
 }
--- Phone Selling System/PhoneSystemTesting/Customer/tstCustomer.cs
+++ Phone Selling System/PhoneSystemTesting/Customer/tstCustomer.cs
@@ -3,11 +3,207 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
+
+
+
+//Dbo.tblCustomer Values
+//[CustID]
+//[Address]
+//[DOB]     
+//[Name]  
+//[Email] 
+//[PhoneNo]
 namespace PhoneSystemTesting
 {
     [TestClass]
     public class tstCustomer
     {
+
+        string Address = "aa";
+        string DOB = "aa";
+        string Name = "aa";
+        string Email = "aa";
+        string PhoneNo = "aa";
+
+        
+
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestStockIDFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //check the property
+            if (AnCustomer.CustID != 1)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+        [TestMethod]
+        public void TestStockNameFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //check the property
+            if (AnCustomer.Address != "1")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+        [TestMethod]
+        public void TestWarehouseNoFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //check the property
+            if (AnCustomer.DOB != "1")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+        [TestMethod]
+        public void TestQuantityFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //check the property
+            if (AnCustomer.Name != "1")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+
+        [TestMethod]
+        public void TestBarcodeFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //check the property
+            if (AnCustomer.Email != "1")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+
+        [TestMethod]
+        public void TestLocationFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustID = 1;
+            //invoke the method
+            Found = AnCustomer.Find(CustID);
+            //check the property
+            if (AnCustomer.PhoneNo != "1")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         [TestMethod]
         public void InstanceOK()
         {
@@ -92,9 +288,9 @@ namespace PhoneSystemTesting
             //create some test data to assign to the property
             string TestData = "LE1 4AB";
             //assign the data to the property
-            ACustomer.PhoneN = TestData;
+            ACustomer.PhoneNo = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(ACustomer.PhoneN, TestData);
+            Assert.AreEqual(ACustomer.PhoneNo, TestData);
         }
 
 
