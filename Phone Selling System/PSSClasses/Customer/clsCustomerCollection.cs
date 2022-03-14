5fa5c2e2f017c9503670de34f5783a4266ec80b0
Mahammad Memon <P2629099@my365.dmu.ac.uk>
2022-03-14 17:35:47 +0000

    Collection class for week 23

---
+++ Phone Selling System/PSSClasses/Customer/clsCustomerCollection.cs
@@ -0,0 +1,207 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PSSClasses
+{
+    public class clsCustomerCollection
+    {
+
+
+       
+            //private data member for the list
+            List<clsCustomer> mCustList = new List<clsCustomer>();
+            public int Counts { get; set; }
+            clsCustomer mThisCust = new clsCustomer();
+            public int Add()
+            {
+                //adds a new record to the database based on the values
+                //set the primary key value of the new record
+                clsDataConnection DB = new clsDataConnection();
+
+                DB.AddParameter("@PhoneNo", mThisCust.PhoneNo);
+                DB.AddParameter("@Address", mThisCust.Address);
+                DB.AddParameter("@DOB", mThisCust.DOB);
+                DB.AddParameter("@Name", mThisCust.Name);
+                DB.AddParameter("@Email", mThisCust.Email);
+                //execute the query returning the primary key value
+                return DB.Execute("sproc_tblCust_Insert");
+
+            }
+            public clsCustomer ThisCust
+            {
+                get
+                {
+                    //return the private data
+                    return mThisCust;
+                }
+                set
+                {
+                    //set private data
+                    mThisCust = value;
+                }
+            }
+
+
+            public List<clsCustomer> CustList
+            {
+                get
+                {
+                    //return the private data
+                    return mCustList;
+                }
+                set
+                {
+                    //set the private data
+                    mCustList = value;
+                }
+            }
+
+            public int Count
+            {
+                get
+                {
+                    //return the count of the list
+                    return mCustList.Count;
+                }
+                set
+                {
+                    //we shall worry about this later
+                }
+            }
+
+
+            public clsCustomerCollection()
+            {
+
+                //objcet for data connection
+                clsDataConnection DB = new clsDataConnection();
+                //execute the stored procedure
+                DB.Execute("sproc_tblCust_SelectAll");
+                //populate the array list with data table
+                PopulateArray(DB);
+
+            }
+
+            public void Update()
+            {
+                //adds a new record to the database based on the values
+                //set the primary key value of the new record
+                clsDataConnection DB = new clsDataConnection();
+                DB.AddParameter("@CustID", mThisCust.CustID);
+                DB.AddParameter("@PhoneNo", mThisCust.PhoneNo);
+                DB.AddParameter("@Address", mThisCust.Address);
+                DB.AddParameter("@DOB", mThisCust.DOB);
+                DB.AddParameter("@Name", mThisCust.Name);
+                DB.AddParameter("@Email", mThisCust.Email);
+                //execute the query returning the primary key value
+                DB.Execute("sproc_tblCust_Update");
+
+            }
+
+            void PopulateArray(clsDataConnection DB)
+            {
+                //populate the array list based on the data table in the parameter DB
+                //var for the index
+                int Index = 0;
+                //var to store the record count 
+                int RecordsCount = 0;
+                //get the count of records
+                RecordsCount = DB.Count;
+                //clear the private array list
+                mCustList = new List<clsCustomer>();
+                //while there are records to process
+                while (Index < RecordsCount)
+                {
+                    //create a blank book
+                    clsCustomer AnCustomer = new clsCustomer();
+                    //read in the fields from the current records
+                    AnCustomer.CustID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustID"]);
+                    AnCustomer.Address = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                    AnCustomer.DOB = Convert.ToString(DB.DataTable.Rows[Index]["DOB"]);
+                    AnCustomer.Name = Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+                    AnCustomer.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                    AnCustomer.PhoneNo = Convert.ToString(DB.DataTable.Rows[Index]["PhoneNo"]);
+
+
+
+                    //add the record to the private data member
+                    mCustList.Add(AnCustomer);
+                    //point at the record
+                    Index++;
+                }
+            }
+
+
+            public void Delete()
+            {
+                //delete the record pointed by the thisStock
+                //connects database
+                clsDataConnection DB = new clsDataConnection();
+                //parameters for the stored procedure
+                DB.AddParameter("@CustID", mThisCust.CustID);
+                //execute the stored procedure 
+                DB.Execute("sproc_tblCust_Del");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Phone Selling System/PhoneSystemTesting/Customer/tstCustomerCollection.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSSClasses;
+
+
+
+namespace PhoneSystemTesting
+{
+
+
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        //Dbo.tblCustomer Values
+        //[CustID]
+        //[Address]
+        //[DOB]     
+        //[Name]  
+        //[Email] 
+        //[PhoneNo]
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //test to see that it exist;
+            Assert.IsNotNull(AllCustomers);
+        }
+
+        [TestMethod]
+        public void CustListOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list objects
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //add an item to the list
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.CustID = 1;
+            TestItem.Address = "abracadabra";
+            TestItem.DOB = "abracadabra";
+            TestItem.Name = "abracadabra";
+            TestItem.Email = "abracadabra";
+            TestItem.PhoneNo = "abracadabra";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            AllCustomers.CustList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomers.CustList, TestList);
+        }
+
+        //[CustID]
+        //[Address]
+        //[DOB]     
+        //[Name]  
+        //[Email] 
+        //[PhoneNo]
+        [TestMethod]
+        public void ThisCustPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            //create some test data to assign to the property
+            clsCustomer TestStock = new clsCustomer();
+            //set the properties of the test object
+            TestStock.CustID = 1;
+            TestStock.Address = "abracadabra";
+            TestStock.DOB = "abracadabra";
+            TestStock.Name = "abracadabra";
+            TestStock.Email = "abracadabra1";
+            TestStock.PhoneNo = "abracadabra";
+            //assign the data to the property
+            AllCustomers.ThisCust = TestStock;
+            Assert.AreEqual(AllCustomers.ThisCust, TestStock);
+
+        }
+
+
+
+
+
+
+
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.CustID = 1;
+            TestItem.Address = "abracadabra";
+            TestItem.DOB = "abracadabra";
+            TestItem.Name = "abracadabra";
+            TestItem.Email = "abracadabra";
+            TestItem.PhoneNo = "abracadabra";
+            //set ThisStock to the test data
+            AllCustomers.ThisCust = TestItem;
+            //add the record
+            PrimaryKey = AllCustomers.Add();
+            //set the primary key of the test data
+            TestItem.CustID = PrimaryKey;
+            //find the record
+            AllCustomers.ThisCust.Find(PrimaryKey);
+            //test to see the two values are the same
+            Assert.AreEqual(AllCustomers.ThisCust, TestItem);
+
+        }
+
+
+
+        [TestMethod]
+        public void DelMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.CustID = 1;
+            TestItem.Address = "abracadabra";
+            TestItem.DOB = "abracadabra";
+            TestItem.Name = "abracadabra";
+            TestItem.Email = "abracadabra";
+            TestItem.PhoneNo = "abracadabra";
+            //set ThisStock to the test data
+            AllCustomers.ThisCust = TestItem;
+            //add the record
+            PrimaryKey = AllCustomers.Add();
+            //set the primary key of the test data
+            TestItem.CustID = PrimaryKey;
+            //find the record
+            AllCustomers.ThisCust.Find(PrimaryKey);
+            //test to see the two values are the same
+            AllCustomers.Delete();
+            Assert.AreEqual(AllCustomers.ThisCust, TestItem);
+
+        }
+
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            //create some test data to assign for the property
+            //in this case the data need to be a list of objects
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //add an item to the list
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.CustID = 1;
+            TestItem.Address = "abracadabra";
+            TestItem.DOB = "abracadabra";
+            TestItem.Name = "abracadabra";
+            TestItem.Email = "abracadabra";
+            TestItem.PhoneNo = "abracadabra";
+
+            //add the item to the property
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCustomers.CustList = TestList;
+            //test to see that two values are the same
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+        }
+
+
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create ab instance of teh class we want to create
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.Address = "abracadabra";
+            TestItem.DOB = "abracadabra";
+            TestItem.Name = "abracadabra";
+            TestItem.Email = "abracadabra";
+            TestItem.PhoneNo = "abracadabra";
+            //set thisstock to the test data
+            AllCustomers.ThisCust = TestItem;
+            //add the record
+            PrimaryKey = AllCustomers.Add();
+            //set the primary key of the test data
+            TestItem.CustID = PrimaryKey;
+            //modify the test data
+            TestItem.Address = "abracadabra";
+            TestItem.DOB = "abracadabra";
+            TestItem.Name = "abracadabra";
+            TestItem.Email = "abracadabra";
+            TestItem.PhoneNo = "abracadabra";
+            //set the record based on the new test data
+            Assert.AreEqual(AllCustomers.ThisCust, TestItem);
+        }
+
+
+    }
+
+}
