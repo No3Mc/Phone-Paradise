13926e4d48a1c74b55c179837bfe8486cb88c94e
LLydiatt <P2606530@my365.dmu.ac.uk>
2022-03-24 08:54:50 +0000

    Added testing for staff class

---
+++ Phone Selling System/PhoneSystemTesting/Staff/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSSClasses;
+using System;
+
+namespace PhoneSystemTesting.Staff
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance od the class
+            clsStaff AStaff = new clsStaff();
+            //test to see if it exsists
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
